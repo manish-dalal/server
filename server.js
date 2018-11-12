80a1e250e25f0ed49441db985827e19be1a9638c
Manish Dalal <md4640@gmail.com>
2018-11-12 11:43:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var cors = require("cors");
+// var cors = require("cors");
 
 const path = require("path");
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cors());
 
 app.post("/media", function(req, res) {
-  console.log("data", req);
+//   console.log("data", req);
 
   res.send("ok");
 });
